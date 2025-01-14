b49277e9bdba27486b62627f769f1feba51f1260
cari1412 <vadis1766@gmail.com>
2025-01-14 22:07:53 +0300

    as

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/traii/',  // добавьте эту строку здесь
   plugins: [react()],
 })
